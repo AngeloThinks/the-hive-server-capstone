5a23a8a7bffe9ee61d812894f1f617a3206b7e65
Angelo Vazquez <angelovazquez@Angelos-MacBook-Pro.local>
2021-01-30 14:15:00 -0600

    deploy

--- src/app.js
+++ src/app.js
@@ -31,7 +31,7 @@ app.use('/hiveapi/appointment',appointmentRouter);
 app.use(function errorHandler(error, req, res, next) {
   let response;
   if (NODE_ENV === 'production') {
-    response = { error: { message: 'server error' } };
+    response = { error: { message: error } };
   } else {
     response = { message: error.message, error };
   }
