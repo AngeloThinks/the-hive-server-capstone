0aee0a3bbbf61342f3a8f228ae4de7107c4de954
Angelo Vazquez <angelovazquez@Angelos-MacBook-Pro.local>
2021-01-30 14:57:58 -0600

    ssl

--- postgrator-config.js
+++ postgrator-config.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 module.exports = {
     migrationDirectory: "migrations",
     driver: "pg",
-    // ssl: "true",
+    ssl: "true",
     host: process.env.MIGRATION_DB_HOST,
     port: process.env.MIGRATION_DB_PORT,
     database: process.env.MIGRATION_DB_NAME,
