12fe4ead6b2974e1592afffe27d8a6d7b0124d82
Angelo Vazquez <angelovazquez@Angelos-MacBook-Pro.local>
2021-01-30 11:48:49 -0600

    seeds

--- seeds/seed.hiveapi_tables.sql
+++ seeds/seed.hiveapi_tables.sql
@@ -1,4 +1,5 @@
 BEGIN;
+
 INSERT INTO hiveapi_users (user_name,first_name,last_name ,password,email,phone_number )
 VALUES
   ('demoacc',
@@ -8,21 +9,17 @@ VALUES
   '$2a$12$zjujb7acz65IE0DOM1JJ6.iedBs1Opou6.9ADYHW.DwbfxQsw.niG',
   'demoemail@.com',
   '123-123-1234');
-
-
 INSERT INTO engineers(first_name)
 VALUES
   ('Angelo'),
   ('Ross'),
   ('Christian'),
   ('Pat');
-
 INSERT INTO services (type , price)
 VALUES
   ('Single song mastered','$20'),
   ('Two songs mastered','$25'),
   ('Three songs mastered','$30'),
   ('Four songs mastered','$35');
-
 COMMIT;
 
